a7ae824201d67c4842f1c43b296413a8198425de
Thamires <thamiipie@gmail.com>
2024-04-04 18:38:58 -0300

    criação dos botões da pagina da conta (e suas respectivas paginas)

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,11 @@
 
 import { BrowserRouter,  Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Conta from "./pages/Conta";
+import Deposito from "./pages/Deposito";
+import Saque from "./pages/Saque";
+import Transacoes from "./pages/Deposito";
+
 // import Conta from "./pages/conta";
 // <Route path="/conta" element={<Conta />} />  . PASSAR PARA O RETURN QUANDO TERMINAR CONTA
 export default function AppRouter(){
@@ -8,7 +13,10 @@ export default function AppRouter(){
         <BrowserRouter>
         <Routes> 
             <Route path="/" element={<Home />} />
-            
+            <Route path="/conta" element={<Conta />} />
+            <Route path="/saque" element={<Saque />} />
+            <Route path="/Deposito" element={<Deposito />} />
+            <Route path="/Transacoes" element={<Transacoes />} />
         </Routes>
          </BrowserRouter>
     )
