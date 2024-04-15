9075390a770f5bb869c574d24383bf02c5c797e8
Thamires <thamiipie@gmail.com>
2024-04-15 16:09:45 -0300

    estilizações

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,10 @@
 
 
 import "../Button.css"
+import depositar from "../assets/depositar.png";
+import retirar from "../assets/retirar.png";
+import transacao from "../assets/transacoes.png";
+
 
 interface ButtonProps{
     text: string
@@ -8,6 +12,16 @@ interface ButtonProps{
 export default function Button({text}:ButtonProps){
     return <button>{text}</button>
 
-
 }
 
+// export function ButtonDeposit({text}:ButtonProps){
+//     return <button>{text}<img src={depositar}/></button>
+// }
+
+// export function ButtonWithdraw({text}:ButtonProps){
+//     return <button>{text}<img src={retirar}/></button>
+// }
+
+// export function ButtonTransactions({text}:ButtonProps){
+//     return <button>{text}<img src={transacao}/></button>
+// }
--- src/pages/Conta.tsx
+++ src/pages/Conta.tsx
@@ -18,13 +18,15 @@
 // }
   
 import { Link } from "react-router-dom";
-import Button from "../components/Button";
+import  Button from "../components/Button";
 import { useContext, useEffect } from 'react'
 import axios from "axios";
 import devimagem from "../assets/devimagem.png";
 import { Dados_Conta } from "../contextoDevBank/contextoConta";
 import "./Conta.css";
-
+// import depositar from "../assets/depositar.png";
+// import retirar from "../assets/retirar.png";
+// import transacao from "../assets/transacao.png";
 export default function Home() {
  
   const { setName, setAgency, setAccount, setCurrent_Balance, api, name, agency, account, current_balance} = useContext(Dados_Conta)  
@@ -34,33 +36,36 @@ export default function Home() {
     setAgency(response.data.agency)
     setAccount(response.data.account)
     setCurrent_Balance(response.data.current_balance)
+    console.log(name)
   }
   
   useEffect(()=> {
     chamaApi()
+    
   },[])
 
   return (
     <main
     className="conta">
       <div> 
+      <img className="imagem-conta"
+      src= {devimagem}/>
       <div className = "box-dados-conta">
         <h2 className="dados-conta">Nome: {name}<br/>
             Agencia: {agency}<br/>
             Conta: {account}<br/>
-            Saldo: {current_balance}<br/>    
+            Saldo atual: {current_balance}<br/>    
       </h2>
       </div>
       </div>
-      <img className="imagem"
-      src= {devimagem}/>
       
+      {/* <img src={depositar}/> */}
       
-      <h1>Selecione o que deseja fazer:</h1>
+      <h1 className="texto-conta"> Selecione o que deseja fazer: </h1>
       
       <div>
         <Link to={"/depositar"} >  
-        <Button text="Despositar" />
+        <Button text="Despositar"/>
         </Link>
         
         <Link to={"/sacar"}>  
